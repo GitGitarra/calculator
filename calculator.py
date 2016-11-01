75fdf45f727223ff8e8d93d32ffd84d50b9600c6
GitGitarra <monika.plocica@gmail.com>
2016-11-01 21:25:06 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,36 +6,28 @@ operators = ["+", "-", "*", "/"]    #we are doing a list of operators we can use
 while running:
 
     try:
-        operand1 = int(input("Enter first number (or letter if you are already" 
-                             "giving up and wish to exit):"))    
+        operand1 = int(input("Enter a number (or letter to exit):"))    
                              #if it won't be a number program will exit
     except ValueError:
-        print("Goodby and see you next time!")
         exit()
-    op = input("Enter an operator (+,-,* or /):")
+    op = input("Enter an operation:")
     if op in operators:   #we are checking if the operator we use is on the list of operators
         try:
-            operand2 = int(input("Enter second number:"))    #we can't put anything else than number
+            operand2 = int(input("Enter another number:"))    #we can't put anything else than number
         except ValueError:
-            print("Oups! That's not a number...")
-            print("Don't give up, try again!")
+            print("Invalid sign")
             continue
         if op == "+":    #calculator is checking which operator was used
             result = operand1 + operand2
-            print(operand1, "+", operand2, "=", result)
-            print("I'm ready to do some more math!")
+            print("Result:", result)
         elif op == "-":
             result = operand1 - operand2
-            print(operand1, "-", operand2, "=", result)
-            print("I'm ready to do some more math!")
+            print("Result:", result)
         elif op == "*":
             result = operand1 * operand2
-            print(operand1, "*", operand2, "=", result)
-            print("I'm ready to do some more math!")
+            print("Result:", result)
         elif op == "/":
             result = operand1 / operand2
-            print(operand1, "/", operand2, "=", result)
-            print("I'm ready to do some more math!")
+            print("Result:", result)
     else:
-        print("It wasn't operator i expect...")    #if we make a mistake the calculator will give us another chance
-        print("Don't give up, try again!")
+        print("Invalid sign")    #if we make a mistake the calculator will give us another chance
